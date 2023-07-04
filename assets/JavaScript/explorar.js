335b437bdcc3bb5a479d3386ffa6154967774cf9
thaizerbinatti <thatazerb@gmail.com>
2023-07-04 00:44:02 -0300

    pagina explorar conteudo finalizado

--- assets/JavaScript/explorar.js
+++ assets/JavaScript/explorar.js
@@ -1,3 +1,4 @@
+// Ver mais e ver menos
 var verMaisLink = document.getElementById("ver-mais-link");
 var conteudoOculto = document.getElementById("conteudo-oculto");
 
@@ -11,3 +12,16 @@ verMaisLink.addEventListener("click", function (event) {
         verMaisLink.textContent = "ver mais";
     }
 });
+
+// Scroll categorias
+const opcoesCategorias = document.getElementsByClassName('opcoesCategorias');
+for (let i = 0; i < opcoesCategorias.length; i++) {
+    opcoesCategorias[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        const sectionId = this.getAttribute('href').substring(1);
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+}
