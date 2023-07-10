650298e76b32e9580336667fbc0ec830508c3fa7
thaizerbinatti <thatazerb@gmail.com>
2023-07-10 12:07:37 -0300

    caminho de imagens e função alert arrumadas

--- assets/JavaScript/servicos.js
+++ assets/JavaScript/servicos.js
@@ -3,14 +3,14 @@ var contador = 0;
 
 var botoes = document.getElementsByClassName("incrementar-carrinho");
 for (var i = 0; i < botoes.length; i++) {
-  botoes[i].addEventListener("click", function() {
-    contador++;
-    atualizarContador();
-  });
+    botoes[i].addEventListener("click", function () {
+        contador++;
+        atualizarContador();
+    });
 }
 
 function atualizarContador() {
-  document.getElementById("contador").textContent = contador;
+    document.getElementById("contador").textContent = contador;
 }
 
 var btn = document.querySelector("#back-to-top");
@@ -40,23 +40,43 @@ formulario.addEventListener('submit', function (e) {
 
 function mostrarMensagemEnviado() {
     const mensagem = document.createElement('p');
-    mensagem.textContent = 'Enviado com sucesso';
-    mensagem.style.color = 'green';
+    mensagem.textContent = 'Enviado com sucesso! Aguarde mais informações por email.';
+    mensagem.className = 'alert alert-success mt-2';
     formulario.appendChild(mensagem);
+
+    setTimeout(function () {
+        mensagem.remove();
+    }, 4000);
+
+    formulario.reset();
 }
 
 // Formulário do projeto social
-document.getElementById('enviar-email').addEventListener('click', function (event) {
-    event.preventDefault(); // Evita o envio do formulário
-
-    var email = document.getElementById('email').value;
-    var mensagemDiv = document.getElementById('mensagem');
-
-    if (email !== '') {
-        var mensagem = 'Obrigada, aguarde o link do formulário no seu email.';
-        mensagemDiv.innerHTML = mensagem;
-    }
+const btnEnviar = document.getElementById('enviar-email');
+btnEnviar.addEventListener('click', function (e) {
+    e.preventDefault();
+    mostrarAlertaEnviado();
+    limparCampoEmail();
 });
+
+function mostrarAlertaEnviado() {
+    const alerta = document.createElement('div');
+    alerta.className = 'alert alert-success mt-2';
+    alerta.textContent = 'Obrigada! Aguarde em seu email os envios dos formulários!';
+
+    const lista = document.querySelector('.list-unstyled');
+    lista.appendChild(alerta);
+
+    setTimeout(function () {
+        alerta.remove();
+    }, 3000);
+
+}
+
+function limparCampoEmail() {
+    const campoEmail = document.getElementsByClassName('email-limpar')[0];
+    campoEmail.value = '';
+}
 
 
 
